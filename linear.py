321c2ae933f098eb592a97c06135479a5303168a
zrenyx <rez-qy@hotmail.com>
2025-05-30 16:23:58 +0100

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+# Load data from CSV file
+data = pd.read_csv('home_dataset.csv')
+ 
+# Extract features and target variable
+house_sizes = data ['HouseSize'].values
+house_prices = data['HousePrice'].values
+
+# Visualize data with graph
+plt.scatter(house_sizes, house_prices, marker ='o', color ='blue')
+plt.title('House Prices vs House size')
+plt.xlabel('House Size (sq.ft)')
+plt.ylabel('House Price ($)')
+plt.show()
+
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(house_sizes, house_prices, test_size=0.2, random_state=42)
+
+# Reshape data for NumPy
+x_train = x_train.reshape(-1, 1)
+x_test = x_test.reshape(-1, 1)
+
+# Create and train model
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+# Predict prices for test
+predictions = model.predict(x_test)
+
+# Visualize the predictions
+plt.scatter(x_test, y_test, marker='o', color='blue', label='Actual Prices')
+plt.plot(x_test, predictions, color='red', linewidth=2, label='Predicted Price')
+plt.title("Property Price Prediction with Linear Regression")
+plt.xlabel('House Size (sq.ft)')
+plt.ylabel('House Price (millions $)')
+plt.legend()
+plt.show()
+
